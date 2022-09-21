9a55ccc2e7139e0f447bfbf561435423edb85441
Alickx <alickx@foxmail.com>
2022-09-22 00:32:37 +0800

    更新文件

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
 
-@app.route("/",methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
@@ -17,5 +18,6 @@ def users():
 def user(id):
     return jsonify(data={'name': 'test1'})
 
+
 # HTTP 直通函数由于是基于 docker 镜像运行，所以必须监听地址为 0.0.0.0，并且端口为 9000
-app.run(host = '0.0.0.0', port = 9000)
+app.run(host='0.0.0.0', port=9000)
