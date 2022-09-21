0d96f32792fbecca39b2350080569788533f0b82
Alickx <alickx@foxmail.com>
2022-09-21 23:57:41 +0800

    更新文件

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask.flask import request
 app = Flask(__name__)
 
 
-@app.route("/chat")
+@app.route("/chat", methods=["POST"])
 def chat():
-    challenge = request.args.get("challenge")
+    challenge = request.json.get("CHALLENGE")
     return challenge
 
 # HTTP 直通函数由于是基于 docker 镜像运行，所以必须监听地址为 0.0.0.0，并且端口为 9000
