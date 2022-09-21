063a4d7056dd52174a19dd2978789f29b41cfb19
Alickx <alickx@foxmail.com>
2022-09-22 01:27:46 +0800

    更新文件

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template('templates/index.html')
 
 
 @app.route("/chat", methods=['POST'])
@@ -16,3 +11,6 @@ def chat():
 
 # HTTP 直通函数由于是基于 docker 镜像运行，所以必须监听地址为 0.0.0.0，并且端口为 9000
 app.run(host='0.0.0.0', port=9000)
+
+if __name__ == '__main__':
+    app.run()
