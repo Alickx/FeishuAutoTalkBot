205de6294eec1561a3e0d3c585d76d783ff672ad
Alickx <alickx@foxmail.com>
2022-12-07 15:21:25 +0800

    更新代码

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def chat():
     challenge = request.json['challenge']
     # 发送get请求
     url = os.getenv('HANDLE_URL')
-    requests.post(url, data=request.json)
+    requests.post(url, data=json.dumps(request.json))
     data = {
         "challenge": challenge
     }
