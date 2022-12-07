c4bd3aee7575839d50e2a2430c6a28ed11e89f9b
Alickx <alickx@foxmail.com>
2022-12-07 15:02:43 +0800

    更新代码

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import os
 from concurrent.futures import ThreadPoolExecutor
 
+import requests
 from flask import Flask, jsonify, render_template, request, url_for, send_from_directory
 from service import ContentDistributor
 from werkzeug.utils import secure_filename
@@ -22,10 +23,17 @@ def test():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    data = request.json['event']
-    # 异步处理消息
-    ContentDistributor.distributeContent(data)
-    return 'success'
+    # data = request.json['event']
+    # # 异步处理消息
+    # ContentDistributor.distributeContent(data)
+    # return 'success'
+    challenge = request.json['challenge']
+    # 发送get请求
+    url = os.getenv('HANDLE_URL')
+    requests.post(url, data=json.dumps(request.json))
+    return {
+        "challenge": challenge
+    }
 
 
 # 启动服务，监听 9000 端口，监听地址为 0.0.0.0
