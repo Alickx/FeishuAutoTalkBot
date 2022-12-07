fcb02595b911db43f019a87cd784d0422001c42d
Alickx <alickx@foxmail.com>
2022-12-07 15:29:01 +0800

    更新代码

--- app.py
+++ app.py
@@ -20,18 +20,9 @@ def test():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    # data = request.json['event']
-    # # 异步处理消息
-    # ContentDistributor.distributeContent(data)
-    # return 'success'
-    challenge = request.json['challenge']
-    # 发送get请求
     url = os.getenv('HANDLE_URL')
     requests.post(url, data=json.dumps(request.json))
-    data = {
-        "challenge": challenge
-    }
-    return json.dumps(data)
+    return 'success'
 
 
 # 启动服务，监听 9000 端口，监听地址为 0.0.0.0
